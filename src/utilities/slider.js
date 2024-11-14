9d9b65025edb53c9dbeba66ef8fb9bb9695ef0bf
rts-Liva <U2451012@rts-365.dk>
2024-11-14 14:13:36 +0100

    fix js

---
+++ src/utilities/slider.js
@@ -0,0 +1,38 @@
+const container = document.querySelector('#slider');
+const sliderRoll = document.querySelector('#slider__roll');
+
+const next = document.querySelector('#slider__next');
+const dots = document.querySelector('#slider__dots');
+
+Array.from(sliderRoll.children).forEach((index) => {
+    dots.innerHTML += `<div class="dot ${index == 0 ? 'fill' : ''}"></div>`
+})
+
+let currentIndex = 0;
+const sliderWindowWidth = sliderRoll.offsetWidth;
+
+function sliderDots() {
+    Array.from(dots.children).forEach((dot, index) => {
+        if (currentIndex === index) {
+            dot.classList.add('current-dot')
+        } else {
+            dot.classList.remove('current-dot')
+        }
+    })
+}
+
+next.addEventListener('click', () => {
+    let newIndex = currentIndex
+    
+    if (newIndex < sliderRoll.children.length - 1) {
+        newIndex = currentIndex + 1
+    }
+
+    const offset = newIndex * sliderWindowWidth
+    sliderRoll.animate([{ transform: `translateX(-${offset}px)` }], { duration: 500, fill: 'both' })
+    currentIndex = newIndex
+
+    sliderDots();
+})
+
+sliderDots();
